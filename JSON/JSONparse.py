d77a3dd8aff161de0fda391163b8ab2a4aae728e
Sean MacEachern <sean.r.maceachern@gmail.com>
2022-09-19 12:44:34 -0400

    added color to the ASCII text as well added a goodbye banner

--- JSON/JSONparse.py
+++ JSON/JSONparse.py
@@ -3,6 +3,7 @@ import sys
 import json
 import time
 import pyfiglet
+from rich import print
 # In order to run this file make sure to add executable privileges then run in shell ./JSONparse.py log.json
 
 def srcIPcount(diction): # takes the documented logs into the function
@@ -17,9 +18,9 @@ def srcIPcount(diction): # takes the documented logs into the function
     max_si_value = max(ip_dict.values()) # looks for the most common IP
     for key,value in ip_dict.items(): # for each key, value pair in dictionary
         if value == max_si_value: # if the value is the same as the most common IP
-            print("*" * 100)
-            print("    ***           The Occurence of {0} in the adbhoney.json file: {1}                 ***".format(key, value))
-            print("*" * 100)
+            print(f'[red]{"*" * 133}[/red]')
+            print(" " * 10, f'[red]{"    ***           "}[/red]', "The occurence of {0} in the adbhoney.json file: {1}".format(key, value), f'[blue]{"                 ***"}[/blue]')
+            print(f'[blue]{"*" * 133}[/blue]')
             print()
             time.sleep(4)
 
@@ -89,51 +90,88 @@ def main():
         
         conStruct = {key:value for key,value in zip(conOrder, conDict)} # merges the key values into a dictionary
         conList.append(conStruct) # appends the merged key value pairs into the List
+    goodbyer = """
+
+
+                     $$$$$$\                            $$\                     $$$$$$$\ $$\     $$\ $$$$$$$$\ 
+                    $$  __$$\                           $$ |                    $$  __$$\\$$\   $$  |$$  _____|
+                    $$ /  \__| $$$$$$\   $$$$$$\   $$$$$$$ |                    $$ |  $$ |\$$\ $$  / $$ |      """
+    goodbyew = """                    $$ |$$$$\ $$  __$$\ $$  __$$\ $$  __$$ |      $$$$$$\       $$$$$$$\ | \$$$$  /  $$$$$\    """
+    goodbyeb = """                    $$ |\_$$ |$$ /  $$ |$$ /  $$ |$$ /  $$ |      \______|      $$  __$$\   \$$  /   $$  __|   
+                    $$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |                    $$ |  $$ |   $$ |    $$ |      
+                    \$$$$$$  |\$$$$$$  |\$$$$$$  |\$$$$$$$ |                    $$$$$$$  |   $$ |    $$$$$$$$\ 
+                     \______/  \______/  \______/  \_______|                    \_______/    \__|    \________|
+                                                                                           
     
+    """
+
+    titler = """    
+
+
+ $$$$$$\  $$\                           $$\ $$$$$$$\             $$\     $$$$$$$\   $$$$$$\  $$$$$$\ $$$$$$\ $$$$$$\ $$$$$$\ $$$$$$\ 
+$$  __$$\ $$ |                          $$ |$$  __$$\            $$ |    $$  __$$\ $$  __$$\ \_$$  _|\_$$  _|\_$$  _|\_$$  _|\_$$  _|
+$$ /  \__|$$ | $$$$$$\  $$\   $$\  $$$$$$$ |$$ |  $$ | $$$$$$\ $$$$$$\   $$ |  $$ |$$ /  $$ |  $$ |    $$ |    $$ |    $$ |    $$ |  """
+    titlew = """$$ |      $$ |$$  __$$\ $$ |  $$ |$$  __$$ |$$$$$$$  |$$  __$$\\_$$  _|  $$$$$$$\ |$$ |  $$ |  $$ |    $$ |    $$ |    $$ |    $$ |  """
+    titleb ="""$$ |      $$ |$$ /  $$ |$$ |  $$ |$$ /  $$ |$$  ____/ $$ /  $$ | $$ |    $$  __$$\ $$ |  $$ |  $$ |    $$ |    $$ |    $$ |    $$ |  
+$$ |  $$\ $$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ | $$ |$$\ $$ |  $$ |$$ |  $$ |  $$ |    $$ |    $$ |    $$ |    $$ |  
+\$$$$$$  |$$ |\$$$$$$  |\$$$$$$  |\$$$$$$$ |$$ |      \$$$$$$  | \$$$$  |$$$$$$$  | $$$$$$  |$$$$$$\ $$$$$$\ $$$$$$\ $$$$$$\ $$$$$$\ 
+ \______/ \__| \______/  \______/  \_______|\__|       \______/   \____/ \_______/  \______/ \______|\______|\______|\______|\______|
+
+
+    """
+    print()
+    print(f'[red]{"*" * 133}[/red]')
+    print(f'[red]{titler}[/red]')
+    print(titlew)
+    print(f'[blue]{titleb}[/blue]')
+    print(f'[blue]{"*" * 133}[/blue]')
+    print()
+
     while True:
+        print(" " * 30, "What would you like to know about this json file? (1-4)")
+        print()            
+        print(" " * 33, "1. Most common IP address to access the server?")
         print()
-        print("*" * 100)
+        print(" " * 33, "2. Longest connected session?")
         print()
-        result = pyfiglet.figlet_format("CloudPotBOIIIII")
-        print(result)
-
-        print("""
-               What would you like to know about this json file? (1-4)
-                    
-                    1. Most common IP address to access the server?
-
-                    2. Longest connected session?
-
-                    3. List of commands used?
-
-                    4. Exit
-        """)
+        print(" " * 33, "3. List of commands used?")
+        print()
+        print(" " * 33, "4. Exit")
         user_choice = input()
         if user_choice == '1':
             srcIPcount(conList)
         elif user_choice == '2':
-            print("*" * 100)
-            print("    ***           The duration of {0} in the adbhoney.json file: {1}           ***".format(ipsrc, duration))
-            print("*" * 100)
+            print(f'[red]{"*" * 133}[/red]')
+            print(" " * 10, f'[red]{"    ***           "}[/red]', "The duration of {0} in the adbhoney.json file: {1}".format(ipsrc, duration), f'[blue]{"           ***"}[/blue]')
+            print(f'[blue]{"*" * 133}[/blue]')
+            print()
             time.sleep(4)
         elif user_choice == '3':
-            print("*" * 100)
-            print("    ***               The List of commands used are as follows:                ***")
-            print("*" * 100)
+            print(f'[red]{"*" * 133}[/red]')
+            print(" " * 10, f'[red]{"    ***               "}[/red]', "The List of commands used are as follows:", f'[blue]{"                ***"}[/blue]')
+            print()
             time.sleep(3)
             print()
             for cmd in inputList:
-                print("*** ",cmd, "***")
+                print(f'[red]{"*** "}[/red]', cmd , f'[blue]{"***"}[/blue]')
                 print()
                 time.sleep(2)
-            print("*" * 100)
+            print(f'[blue]{"*" * 133}[/blue]')
+            print()
         elif user_choice == '4':
+            print()
+            print(f'[red]{"*" * 133}[/red]')
+            print(f'[red]{goodbyer}[/red]')
+            print(goodbyew)
+            print(f'[blue]{goodbyeb}[/blue]')
+            print(f'[blue]{"*" * 133}[/blue]')
+            print()
             return False
         else:
-            print("""
-                Please enter either 1, 2, 3, or 4.
-            """)
-      
+            print(f'[red]{"*" * 133}[/red]')
+            print(" " * 10, f'[red]{"***                        "}[/red]', "Please enter either 1, 2, 3, or 4.", f'[blue]{"                           ***"}[/blue]')
+            print(f'[blue]{"*" * 133}[/blue]')
+            print()
 
 if __name__ == "__main__":
     main()
